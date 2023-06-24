777d12bc6e700c03be719656326daded1b7e8238
Zoltan Farkas <progfarkas@gmail.com>
2023-06-24 20:30:00 +0200

    Upgrade Gradle from 7.5.1 to 8.1.1

--- sample/sample-docker-pgsql/build.gradle.kts
+++ sample/sample-docker-pgsql/build.gradle.kts
@@ -23,7 +23,7 @@ buildscript {
         }
     }
     dependencies {
-        classpath("com.palantir.gradle.docker:gradle-docker:0.34.0")
+        classpath("com.palantir.gradle.docker:gradle-docker:0.35.0")
     }
 }
 
